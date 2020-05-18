a6e914a6884f9b42e61e8d1f92bb7e392c5607c2
bertyn99 <bboulikou@gmail.com>
2020-05-18 16:47:24 +0200

    name

--- quickstart.py
+++ quickstart.py
@@ -1,22 +1,32 @@
 # imports
+import os
+from os.path import join, dirname
 from dotenv import load_dotenv
 from instapy import InstaPy
 from instapy import smart_run
 
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+
+# env
+
+user = os.environ.get("INSTA_USERNAME")
+mdp = os.environ.get("INSTA_PASSWORD")
+print(mdp)
 # login credentials
-insta_username = ''
-insta_password = ''
+insta_username = user
+insta_password = mdp
 
 comments = ['Nice shot! @{}',
-        'I love your profile! @{}',
-        'Your feed is an inspiration :thumbsup:',
-        'Just incredible :open_mouth:',
-        'What camera did you use @{}?',
-        'Love your posts @{}',
-        'Looks awesome @{}',
-        'Getting inspired by you @{}',
-        ':raised_hands: Yes!',
-        'I can feel your passion @{} :muscle:']
+            'I love your profile! @{}',
+            'Your feed is an inspiration :thumbsup:',
+            'Just incredible :open_mouth:',
+            'Love your posts @{}',
+            'Looks awesome @{}',
+            'Getting inspired by you @{}',
+            ':raised_hands: Yes!',
+            'I can feel your passion @{} :muscle:']
 
 # get an InstaPy session!
 # set headless_browser=True to run InstaPy in the background
@@ -25,14 +35,14 @@ session = InstaPy(username=insta_username,
                   headless_browser=False)
 
 with smart_run(session):
-  """ Activity flow """		
-  # general settings		
-  session.set_dont_include(["friend1", "friend2", "friend3"])		
-  
-  # activity		
-  session.like_by_tags(["natgeo"], amount=10)
+    Activity flow
+    # general settings
+    session.set_dont_include(["friend1", "friend2", "friend3"])
 
-  # Joining Engagement Pods
-  session.set_do_comment(enabled=True, percentage=35)
-  session.set_comments(comments)
-  session.join_pods(topic='sports', engagement_mode='no_comments')
+    # activity
+    session.like_by_tags(["natgeo"], amount=10)
+
+    # Joining Engagement Pods
+    session.set_do_comment(enabled=True, percentage=35)
+    session.set_comments(comments)
+    session.join_pods(topic='sports', engagement_mode='no_comments')
